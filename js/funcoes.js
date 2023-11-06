ac83d238b08ce3dfa73564e6015a955b9e128789
Milton Andrade <102178311+Miltonnn@users.noreply.github.com>
2023-11-05 21:32:34 -0300

    Enviando Landing Page

---
+++ js/funcoes.js
@@ -0,0 +1,11 @@
+
+    $('.slider').nivoSlider();
+
+    function imgSlider(anything){
+        document.querySelector('.starbucks').src = anything;
+    }
+
+    function changeCircleColor(color){
+        const circle = document.querySelector('.circle');
+        circle.style.background = color;
+    }
